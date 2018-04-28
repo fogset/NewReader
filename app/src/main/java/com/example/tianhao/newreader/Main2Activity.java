39892ae22f166a183c9f3d3bcbfa5663ffb3dd7e
Tianhao <zhangth1990@hotmail.com>
2018-04-27 23:57:07 -0600

    Added second activity webview

--- app/src/main/java/com/example/tianhao/newreader/Main2Activity.java
+++ app/src/main/java/com/example/tianhao/newreader/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.tianhao.newreader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        WebView webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        Intent intent = getIntent();
+        webView.loadData(intent.getStringExtra("content"), "text/html", "UTF-8");
     }
 }
